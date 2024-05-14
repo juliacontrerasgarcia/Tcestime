d95ecb53186e284c1f8fa8c767564671842e18d6
Trinidad Novoa <tnovoa@papeete.lct.jussieu.fr>
2024-05-14 16:27:57 +0200

    Print models

--- src/get_tc.py
+++ src/get_tc.py
@@ -8,15 +8,18 @@ def get_tc(net_val, h_frac, h_dos, molec=None, fit='meansq'):
     #t_c = 750.0 * phi_dos - 85
     if fit == 'meansq':
         t_c = 429.3 * phi_dos - 10.4
+        eq_tc = "Tc^{meansq} = 429.3 * phi * h_f * (h_dos)^(1/3) - 10.4"
     elif fit == 'SR2':
         t_c = 442.3 * (1- (molec-net_val)) * (h_frac**3) * (h_dos**(1./2))
+        eq_tc = "Tc^{SR2} = 442.3 * (1 - (phis - phi)) * h_f^3 * (h_dos)^(1/2)"
     elif fit == 'SR4':
         t_c = 574.7 * net_val * (h_dos * h_frac**3 )**(1./2)
+        eq_tc = "Tc^{SR4} = 574.7 * phi * ( h_dos * h_f^3 )^(1/2)"
 
-    return t_c
+    return t_c, eq_tc
 
 
 def write_tc(file_tc, net_val, h_frac, h_dos, molec=None, fit='meansq'):
-    tc = get_tc(net_val, h_frac, h_dos, molec, fit)
+    tc, eq_tc = get_tc(net_val, h_frac, h_dos, molec, fit)
     with open(file_tc, "w") as f:
         f.write("{:.2f} \n".format(tc))
