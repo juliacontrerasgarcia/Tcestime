fea729cffe5c6624dfc0543a9ccceafc46d17de6
Trinidad Novoa <trini.novoa@gmail.com>
2024-06-07 17:14:18 +0200

    Add GBR model

--- src/get_tc.py
+++ src/get_tc.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
 import numpy as np
-
+import pickle
+import os
 
 def get_tc(net_val, h_frac, h_dos, molec=None, fit='leastsq'):
     phi_dos = net_val * h_frac * (h_dos**(1/3))
@@ -14,6 +15,15 @@ def get_tc(net_val, h_frac, h_dos, molec=None, fit='leastsq'):
     elif fit == 'SR4':
         t_c = 574.7 * net_val * (h_dos * h_frac**3 )**(1./2)
         eq_tc = "Tc^{SR4} = 574.7 * phi * ( h_dos * h_f^3 )^(1/2)"
+
+    elif fit == 'GBR':
+        this_dir = os.path.dirname(os.path.abspath(__file__))
+        with open(this_dir+"/gradient_boosting_model.pkl", 'rb') as file:
+            model = pickle.load(file)
+        
+        input_data=[[net_val, h_frac, h_dos, molec]]
+        t_c=model.predict(input_data)[0]
+        eq_tc = "Tc^{GBR} is computed using a Gradient Boosting Regression model"
 
     return t_c, eq_tc
 
